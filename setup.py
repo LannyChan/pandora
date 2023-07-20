4f14eb816f083daa695e3d23547101ed62f54e87
Lanny Chan <74071703+lannychan@users.noreply.github.com>
2023-07-20 23:26:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'api': requirements_api,
-        'cloud': ['pandora-cloud~=0.6.4'],
+        'cloud': ['pandora-cloud~=0.6.5'],
     },
     entry_points={
         'console_scripts': [
